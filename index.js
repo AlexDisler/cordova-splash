eb65d2de67c5ada7015ee983fda57be0485e1fff
Bruno C <mail@brunocartier.fr>
2016-03-11 00:18:26 +0100

    Get back sizes from previous version of the repo
    They seem to be missing in my current Cordova project.
    If some are considered as unused, I will be able to remove them later.

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ var getPlatforms = function (projectName) {
         isAdded : fs.existsSync('platforms/ios'),
         splashPath : 'platforms/ios/' + projectName + '/Images.xcassets/LaunchImage.launchimage/',
         splash : [
+            { name: 'Default-568h@2x~iphone.png',           width: 640,  height: 1136 },
+            { name: 'Default-667h.png',                     width: 750,  height: 1334 },
+            { name: 'Default-736h.png',                     width: 1242, height: 2208 },
+            { name: 'Default-Landscape-736h.png',           width: 2208, height: 1242 },
+            { name: 'Default@2x~iphone.png',                width: 640,  height: 960  },
+            { name: 'Default~iphone.png',                   width: 320,  height: 480  },
+
             { name: 'Default-Portrait~iphone.png',          width: 320,  height: 480  },
             { name: 'Default-Portrait@2x~iphone.png',       width: 640,  height: 960  },
             { name: 'Default-Portrait-568h@2x~iphone.png',  width: 640,  height: 1136 },
